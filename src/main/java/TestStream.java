93af27e265be964352de5038d7719738e6a32296
Tang <childe-tang@qq.com>
2018-05-15 02:41:11 +0800

    java8

---
+++ src/main/java/TestStream.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by Candy on 2018/5/15
+ *
+ * 一、stream的三个操作步骤
+ *
+ * 1. 创建Stream
+ * 2. 中间操作
+ *  筛选与切片 filter limit skip distinct
+ *  映射 map flatMap
+ *  排序 sorted
+ * 3. 终止操作
+ */
+public class TestStream {
+
+    @Test
+    public void test1() {
+        List<String> list = new ArrayList<>();
+
+        Stream<String> s1 = list.stream();
+        Stream<String> s2 = list.parallelStream();
+
+        String[] array = new String[0];
+        Stream<String> s3 = Arrays.stream(array);
+
+        Stream<String> s4 = Stream.of("12", "23");
+
+        Stream<Integer> s5 = Stream.iterate(0, x -> x + 2);
+        s5.limit(10).forEach(System.out::println);
+
+        Stream<Double> s6 = Stream.generate(() -> Math.random());
+        s6.limit(5).forEach(System.out::println);
+    }
+
+}
